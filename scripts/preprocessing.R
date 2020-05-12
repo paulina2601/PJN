46256954557cea1ffc85eeee0285c4a4fda61bbf
paulina2601 <paulina.wujkowska@gmail.com>
2020-05-12 11:14:54 +0200

    28-04-2020

---
+++ scripts/preprocessing.R
@@ -0,0 +1,101 @@
+#włączenie bibliotek
+library(tm)
+library(hunspell)
+library(stringr)
+
+#zmiana katalogu roboczego
+workDir <- "C:\\PW\\PJN\\"
+setwd(workDir)
+
+#definicja katalogów projektu
+inputDir <- ".\\data"
+outputDir <- ".\\results"
+scriptsDir <- ".\\scripts"
+workspaceDir <- ".\\workspaces"
+
+#utworzenie katalogu wyjściowego
+dir.create(outputDir, showWarnings = FALSE)
+dir.create(workspaceDir, showWarnings = FALSE)
+
+#utworzenie korpusu dokumentów
+corpusDir <- paste(
+  inputDir,
+  "\\",
+  "Literatura - streszczenia - oryginał",
+  sep = ""
+)
+corpus <- VCorpus(
+  DirSource(
+    corpusDir,
+    pattern = "*.txt",
+    encoding = "UTF-8"
+  ),
+  readerControl = list(
+    language = "pl_PL"
+  )
+)
+
+#usunięcie z tekstów podziału na akapity
+pasteParagraphs <- content_transformer(function(text, char) paste(text, collapse = char))
+corpus <- tm_map(corpus, " ")
+
+#wstępne przetwarzanie
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, content_transformer(tolower))
+stoplistFile <- paste(
+  inputDir,
+  "\\",
+  "stopwords_pl.txt",
+  sep = ""
+)
+stoplist <- readLines(
+  stoplistFile,
+  encoding = "UTF-8"
+)
+corpus <- tm_map(corpus, removeWords, stoplist)
+corpus <- tm_map(corpus, stripWhitespace)
+
+removeChar <- content_transformer(
+  function(x, pattern, replacement) 
+    gsub(pattern, replacement, x)
+)
+
+#usunięcie "em dash" i 3/4 z tekstów
+corpus <- tm_map(corpus, removeChar, intToUtf8(8722), "")
+corpus <- tm_map(corpus, removeChar, intToUtf8(190), "")
+
+#lematyzacja - sprowadzenie do formy podstawowej
+polish <- dictionary(lang = "pl_PL")
+
+lemmatize <- function(text) {
+  simpleText <- str_trim(as.character(text))
+  parsedText <- strsplit(simpleText, split = " ")
+  newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+  for (i in 1:length(newTextVec)){
+    if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+    if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
+  }
+  newText <- paste(newTextVec, collapse = " ")
+  return(newText)
+}
+
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+#usunięcie rozszerzeń z nazw dokumentów
+cutExtensions <- function(document) {
+  meta(document, "id") <- gsub(pattern = "\\.txt$", "", meta(document, "id"))
+  return(document)
+}
+
+corpus <- tm_map(corpus, cutExtensions)
+
+#eksport korpusu przetworzonego do plików tekstowych
+preprocessedDir <- paste(
+  outputDir,
+  "\\",
+  "Literatura - streszczenia - przetworzone",
+  sep = ""
+)
+dir.create(preprocessedDir, showWarnings = FALSE)
+writeCorpus(corpus, path = preprocessedDir)
--- scripts/script_3.R
+++
@@ -1,1 +0,0 @@
-#właczenie bibliotek
